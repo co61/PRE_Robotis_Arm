29de45ce695d3434c354f4a435110d09c4f50a3d
TanBoi <tanguy.boittin@gmail.com>
2021-11-10 08:46:43 +0100

    Correction controle à la manette

--- c++/PRE_Projet/protocol2.0/PRE_Pattern/manette/controle.cpp
+++ c++/PRE_Projet/protocol2.0/PRE_Pattern/manette/controle.cpp
@@ -1,3 +1,14 @@
+/**
+ *
+ *  Controle du robot avec une Manette de console
+ * 
+ *
+ *
+ */
+
+
+
+
 // --- INCLUDE --- //
 #include "controle.hpp"
 
@@ -55,6 +66,7 @@ size_t get_axis_state(struct js_event *event, struct axis_state axes[3])
 
 void manette(){
   Torque_enable_all();
+  profileVelocity(100,100,100,100,100);
   
   int dt_base = 0;
   int dt_bras1 = 0;
@@ -106,34 +118,36 @@ void manette(){
               if (axis < 7){
                   printf("Axis %zu at (%6d, %6d)\n", axis, axes[axis].x, axes[axis].y);
                   if(axis == 0){
-                    if((int(axes[axis].y/3000)>5)||(int(axes[axis].y/3000)<-5))
-                      dt_bras2 = int(axes[axis].y/3000)*3;
+                    if((int(axes[axis].y/3000)>3))
+                      dt_bras2 = int(axes[axis].y/3000)*4;
+                    else if((int(axes[axis].y/3000)<-3))
+                      dt_bras2 = int(axes[axis].y/3000)*5;
                     else
                       dt_bras2 = 0;
 
-                    if((int(axes[axis].x/3000)>5)||(int(axes[axis].x/3000)<-5))
+                    if((int(axes[axis].x/3000)>3)||(int(axes[axis].x/3000)<-3))
                       dt_bras1 = int(axes[axis].x/3000)*4;
                     else
                       dt_bras1 = 0;
                   }
                   else if(axis == 1){
-                    if((int(axes[axis].y/3000)>5)||(int(axes[axis].y/3000)<-5))
-                      dt_base = int(axes[axis].y/3000)*3;
+                    if((int(axes[axis].y/3000)>3)||(int(axes[axis].y/3000)<-3))
+                      dt_base = int(axes[axis].y/3000)*4;
                     else
                       dt_base = 0;
                     if((int(axes[axis].x)) > 0)
-                      dt_pince = -10;
+                      dt_pince = -int(axes[axis].x/3000)*9;
                     else
                       dt_pince = 0;
                     }
-                  else if(axis == 2){
-                    if((int(axes[axis].x/3000)>5)||(int(axes[axis].x/3000)<-5))
-                      dt_bras3 = int(axes[axis].x/3000)*3;
+                  else if(axis == 2){ 
+                    if((int(axes[axis].x/3000)>3)||(int(axes[axis].x/3000)<-3))
+                      dt_bras3 = int(axes[axis].x/3000)*4;
                     else
                       dt_bras3 = 0;
                     
                     if((int(axes[axis].y))  > 0)
-                      dt_pince = 10;
+                      dt_pince = int(axes[axis].y/3000)*9;
                     else
                       dt_pince = 0;
                     }
@@ -147,10 +161,25 @@ void manette(){
     }
     fflush(stdout);
 
-    moteur_dt(dt_base, dt_bras1, dt_bras2, dt_bras3, dt_pince);
+    if(dt_bras1 != 0)
+      mooveBras1(dt_bras1);
+    else if(dt_bras2 != 0)
+      mooveBras2(dt_bras2);
+
+    if(dt_bras3 != 0)
+      mooveBras3(dt_bras3);
+    else if(dt_base != 0)
+      mooveBase(dt_base);
+
+    if(dt_pince != 0)
+      moovePince(dt_pince);
+    //moteur_dt(dt_base, dt_bras1, dt_bras2, dt_bras3, dt_pince);
     
   }
   //Fermeture du joystick
   close(js);
+  profileVelocity(30,30,30,30,30);
   position();
+
+  
 }
